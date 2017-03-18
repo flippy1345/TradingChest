c12a287bc687f21100abbc91b3176debaa5c9a94
Martin Fiedler <martinfiedler@hotmail.de>
2017-03-18 14:45:32 +0100

    add onEnable()

--- TradingChest/src/main/java/de/Aikonsukun/TradingChest/MainTradingChest.java
+++ TradingChest/src/main/java/de/Aikonsukun/TradingChest/MainTradingChest.java
@@ -1,9 +1,15 @@
 package de.Aikonsukun.TradingChest;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MainTradingChest extends JavaPlugin{
+public class MainTradingChest extends JavaPlugin implements Listener{
 
-	
+	@Override
+	public void onEnable() {
+		System.out.println("[TradingChest 1.0] TradingChest is enabled");
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
 
 }
